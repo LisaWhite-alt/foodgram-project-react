14de28c6714176e00f8143709da948f84cf3cadc
LisaWhite-alt <laguna.crema@gmail.com>
2021-10-06 19:10:28 +0300

    fix v.4

--- backend/recipes/filters.py
+++ backend/recipes/filters.py
@@ -1,36 +1,36 @@
-from django_filters import filters
-from django_filters.rest_framework import FilterSet
+from django_filters import rest_framework as filters
 
-from .models import Ingredient, Recipe
+from recipes.models import Ingredient, Tag, Recipe
 
 
-class RecipeFilter(FilterSet):
+class RecipeFilter(filters.FilterSet):
 
-    is_favorited = filters.ModelChoiceFilter(
-        lookup_expr="isnull",
-        queryset=Recipe.objects.all(),
-        method="filter_is_favorited",
+    is_favorited = filters.BooleanFilter(
+        method="filter_is_favorited")
+    is_in_shopping_cart = filters.BooleanFilter(
+        method="filter_is_in_shopping_cart")
+    tags = filters.ModelMultipleChoiceFilter(
+        field_name="tags__slug",
+        to_field_name="slug",
+        queryset=Tag.objects.all(),
     )
-    is_in_shopping_cart = filters.ModelChoiceFilter(
-        lookup_expr="isnull",
-        queryset=Recipe.objects.all(),
-        method="filter_is_in_shopping_cart",
-    )
-    author = filters.AllValuesFilter(field_name="author")
-    tags = filters.AllValuesMultipleFilter(field_name="tags__slug")
 
     class Meta:
         model = Recipe
         fields = ["is_favorited", "is_in_shopping_cart", "author", "tags"]
 
     def filter_is_favorited(self, queryset, name, value):
-        return queryset.filter(favourite__user=self.request.user)
+        if value:
+            return Recipe.objects.filter(favourite__user=self.request.user)
+        return Recipe.objects.all()
 
     def filter_is_in_shopping_cart(self, queryset, name, value):
-        return queryset.filter(purchase__user=self.request.user)
+        if value:
+            return Recipe.objects.filter(purchase__user=self.request.user)
+        return Recipe.objects.all()
 
 
-class IngredientFilter(FilterSet):
+class IngredientFilter(filters.FilterSet):
     name = filters.CharFilter(field_name="name", lookup_expr="icontains")
 
     class Meta:
